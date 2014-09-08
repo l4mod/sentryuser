6f2d8c691c5d2292285090f94510e45132090324
Amitav Roy <reachme@amitavroy.com>
2014-09-08 21:38:05 +0530

    Changed the login button text, it was Focalworks

--- src/views/login.blade.php
+++ src/views/login.blade.php
@@ -11,7 +11,7 @@
             <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password" name="password">
         </div>
         <button type="submit" class="btn btn-success">Login</button>
-        <a href="{{url('user/oauth')}}" class="btn btn-info col-md-push1">Login with Focalworks Email</a>
+        <a href="{{url('user/oauth')}}" class="btn btn-info col-md-push1">Login with Google ID</a>
         {{ Form::close() }}
     </div>
 </div>
